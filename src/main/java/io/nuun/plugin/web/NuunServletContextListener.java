2cfc0f4cf57aef416247dc42206d5876dd019fd1
ejemba <epo.jemba@kametic.com>
2014-08-22 19:22:50 +0200

    first commit

---
+++ src/main/java/io/nuun/plugin/web/NuunServletContextListener.java
@@ -0,0 +1,91 @@
+/**
+ * Copyright (C) 2013 Kametic <epo.jemba@kametic.com>
+ *
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
+ * or any later version
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.nuun.plugin.web;
+
+import io.nuun.kernel.core.Kernel;
+
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.inject.Injector;
+import com.google.inject.servlet.GuiceServletContextListener;
+
+public class NuunServletContextListener extends GuiceServletContextListener
+{
+
+    private Logger         logger = LoggerFactory.getLogger(NuunServletContextListener.class);
+
+    private ServletContext servletContext;
+    private Kernel         kernel;
+
+    @Override
+	public void contextInitialized(ServletContextEvent servletContextEvent)
+    {
+        servletContext = servletContextEvent.getServletContext();
+        super.contextInitialized(servletContextEvent);
+    }
+
+    @Override
+    protected Injector getInjector()
+    {
+
+        List<String> params = new ArrayList<String>();
+        Enumeration<?> initparams = servletContext.getInitParameterNames();
+        while (initparams.hasMoreElements())
+        {
+            String keyName = (String) initparams.nextElement();
+            if (keyName != null && !keyName.isEmpty())
+            {
+                String value = servletContext.getInitParameter(keyName);
+                logger.info("Adding {}={} to NuunServletContextListener.", keyName, value);
+                params.add(keyName);
+                params.add(value);
+            }
+        }
+
+        
+
+        String[] paramsArray = new String[params.size()];
+        params.toArray(paramsArray);
+
+        kernel = Kernel.createKernel(paramsArray).withContainerContext(servletContext). build();
+
+        kernel.init();
+
+        kernel.start();
+
+        return kernel.getMainInjector();
+    }
+
+    @Override
+	public void contextDestroyed(ServletContextEvent servletContextEvent)
+    {
+
+        kernel.stop();
+
+        // Injector injector = (Injector) servletContext.getAttribute(Injector.class.getName());
+
+        super.contextDestroyed(servletContextEvent);
+    }
+}
---
+++ src/main/java/io/nuun/plugin/web/NuunWebPlugin.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright (C) 2013 Kametic <epo.jemba@kametic.com>
+ *
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
+ * or any later version
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.nuun.plugin.web;
+
+import io.nuun.kernel.api.plugin.AbstractPlugin;
+
+import java.net.URL;
+import java.util.Set;
+
+import javax.servlet.ServletContext;
+
+import org.reflections.util.ClasspathHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.inject.servlet.WorkAroundModule;
+
+public class NuunWebPlugin extends AbstractPlugin
+{
+
+    private Logger         logger = LoggerFactory.getLogger(NuunWebPlugin.class);
+    
+    private WorkAroundModule module = null;
+    private Set<URL> additionalClasspath = null;
+
+    @Override
+    public String name()
+    {
+        return "nuun-web-plugin";
+    }
+
+    @Override
+    public Set<URL> computeAdditionalClasspathScan()
+    {
+        ServletContext servletContext = null;
+
+        if (containerContext != null && ServletContext.class.isAssignableFrom(containerContext.getClass()))
+        {
+
+            servletContext = (ServletContext) containerContext;
+            
+            Set<URL> webCPUrls = ClasspathHelper.forWebInfLib(servletContext);
+            URL forWebInfClasses = ClasspathHelper.forWebInfClasses(servletContext);
+            webCPUrls.add(forWebInfClasses);
+            
+          this.additionalClasspath = webCPUrls;
+            
+        }   
+        
+        return  this.additionalClasspath != null ? this.additionalClasspath : super.computeAdditionalClasspathScan() ;
+    }
+}
